0ae06372d163651b94528a43229f784571aa98a5
curso00 <curso00@nomail.com>
2024-11-21 11:42:51 +0100

    Ejemplo simple de lambdas

---
+++ src/main/java/es/santander/ascender/proyecto06/lambda/Datos.java
@@ -0,0 +1,32 @@
+package es.santander.ascender.proyecto06.lambda;
+
+public class Datos {
+    private int edad;
+    private String nombre;
+
+    public Datos() {
+        
+    }
+
+    public  Datos(int edad, String nombre) {
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    
+    
+}
---
+++ src/test/java/es/santander/ascender/proyecto06/lambda/JugamosConLambdasTest.java
@@ -0,0 +1,61 @@
+package es.santander.ascender.proyecto06.lambda;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class JugamosConLambdasTest {
+    private List<Datos> listaATransformar;
+
+    @BeforeEach
+    public void setup() {
+        listaATransformar = new ArrayList<>();
+
+        listaATransformar.add(new Datos(3, "Juan"));
+        listaATransformar.add(new Datos(80, "Antonio"));
+    }
+
+    @Test
+    public void testTransformarConFor() {
+        for (Datos d: listaATransformar) {
+            d.setEdad(d.getEdad() * 3);
+        }
+
+
+        assertEquals(listaATransformar.get(0).getEdad(), 9);
+        assertEquals(listaATransformar.get(1).getEdad(), 240);
+    }
+
+
+    @Test
+    public void testTransformarSinLambdas() {
+        // No quiero hacer esto, sino que se busca aplicar forech del ArrayList
+        // for (Datos d: listaATransformar) {
+        //     d.setEdad(d.getEdad() * 3);
+        // }
+
+        listaATransformar.forEach(new Consumer<Datos>() { 
+            @Override 
+            public void accept(Datos dato) {
+                dato.setEdad(dato.getEdad() * 3);
+            }           
+        });
+        assertEquals(listaATransformar.get(0).getEdad(), 9);
+        assertEquals(listaATransformar.get(1).getEdad(), 240);
+    }
+
+    @Test
+    public void testTransformarConLambdas() {
+        listaATransformar.forEach( 
+            (d) -> d.setEdad(d.getEdad() * 3)
+        );
+
+        assertEquals(listaATransformar.get(0).getEdad(), 9);
+        assertEquals(listaATransformar.get(1).getEdad(), 240);
+    }
+}
